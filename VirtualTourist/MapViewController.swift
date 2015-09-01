74e90ec6dffa7abf1434dec4e60e53ee6a8a44d2
tarioug <menasra@MacBook-Pro-de-Noufel.local>
2015-09-01 23:53:10 +0200

    adding the mapView and possibility to drop pins with a touch and hold gesture (persistence)

---
+++ VirtualTourist/MapViewController.swift
@@ -0,0 +1,165 @@
+//
+//  ViewController.swift
+//  VirtualTourist
+//
+//  Created by Nawfal on 01/09/2015.
+//  Copyright (c) 2015 Noufel Gouirhate. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreData
+
+class MapViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var arrayOfPinsToPersist = [Pin]()
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Edit", style: UIBarButtonItemStyle.Done, target: self, action: "editPins")
+        
+        var longPressRecogniser = UILongPressGestureRecognizer(target: self, action: "handleLongPress:")
+        
+        longPressRecogniser.minimumPressDuration = 0.5
+        mapView.addGestureRecognizer(longPressRecogniser)
+        
+        handleLongPress(longPressRecogniser)
+        
+        restoreMapRegion(false)
+        
+        arrayOfPinsToPersist = fetchAllPins()
+        var annotations = [MKPointAnnotation]()
+        
+        for pin in arrayOfPinsToPersist {
+            
+            // The lat and long are used to create a CLLocationCoordinates2D instance.
+            let coordinate = CLLocationCoordinate2D(latitude: pin.latitude, longitude: pin.longitude)
+            
+            // Here we create the annotation and set its coordiate, title, and subtitle properties
+            var annotation = MKPointAnnotation()
+            annotation.coordinate = coordinate
+            
+            annotations.append(annotation)
+            
+        }
+        
+        // When the array is complete, we add the annotations to the map.
+        self.mapView.addAnnotations(annotations)
+
+    }
+    
+    var filePath: String {
+        let manager = NSFileManager.defaultManager()
+        let url = manager.URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask).first as! NSURL
+        return url.URLByAppendingPathComponent("mapRegionArchive").path!
+    }
+    
+    func saveMapRegion() {
+        let dictionary = [
+            "latitude" : mapView.region.center.latitude,
+            "longitude" : mapView.region.center.longitude,
+            "latitudeDelta" : mapView.region.span.latitudeDelta,
+            "longitudeDelta" : mapView.region.span.longitudeDelta
+        ]
+        
+        NSKeyedArchiver.archiveRootObject(dictionary, toFile: filePath)
+
+    }
+    
+
+    func restoreMapRegion(animated: Bool) {
+        if let regionDictionary = NSKeyedUnarchiver.unarchiveObjectWithFile(filePath) as? [String : AnyObject] {
+        
+            let longitude = regionDictionary["longitude"] as! CLLocationDegrees
+            let latitude = regionDictionary["latitude"] as! CLLocationDegrees
+            let center = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        
+            let longitudeDelta = regionDictionary["latitudeDelta"] as! CLLocationDegrees
+            let latitudeDelta = regionDictionary["longitudeDelta"] as! CLLocationDegrees
+            let span = MKCoordinateSpan(latitudeDelta: latitudeDelta, longitudeDelta: longitudeDelta)
+        
+            let savedRegion = MKCoordinateRegion(center: center, span: span)
+        
+            println("lat: \(latitude), lon: \(longitude), latD: \(latitudeDelta), lonD: \(longitudeDelta)")
+        
+            mapView.setRegion(savedRegion, animated: animated)
+        }
+    }
+
+
+
+    //http://stackoverflow.com/questions/3959994/how-to-add-a-push-pin-to-a-mkmapviewios-when-touching
+    func handleLongPress(gestureRecognizer : UIGestureRecognizer){
+        if gestureRecognizer.state != .Began {
+            return
+        }
+        
+        let touchPoint = gestureRecognizer.locationInView(self.mapView)
+        let touchMapCoordinate = mapView.convertPoint(touchPoint, toCoordinateFromView: mapView)
+        
+        let latitude = touchMapCoordinate.latitude as Double
+        let longitude = touchMapCoordinate.longitude as Double
+        
+        let pinToBeAdded = Pin(latitude: touchMapCoordinate.latitude, longitude: touchMapCoordinate.longitude, context: self.sharedContext)
+        
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = touchMapCoordinate
+        
+        arrayOfPinsToPersist.append(pinToBeAdded)
+        
+        mapView.addAnnotation(annotation)
+        
+        var error: NSError? = nil
+        self.sharedContext.save(&error)
+        if let error = error {
+            println("error saving context: \(error.localizedDescription)")
+        }
+
+    }
+    
+    func fetchAllPins() -> [Pin] {
+        let error: NSErrorPointer = nil
+        
+        // Create the Fetch Request
+        let fetchRequest = NSFetchRequest(entityName: "Pin")
+        
+        // Execute the Fetch Request
+        let results = sharedContext.executeFetchRequest(fetchRequest, error: error)
+        
+        // Check for Errors
+        if error != nil {
+            println("Error in fectchAllActors(): \(error)")
+        }
+        
+        // Return the results, cast to an array of Person objects
+        return results as! [Pin]
+    }
+
+    
+    
+    func editPins() {
+        //1-afficher la view en rouge disant que tu peux supprimer des pins
+        //2-faire monter la carte
+        //3-supprimer les pins de Core Data
+        println("editPins() touched")
+    }
+    
+    lazy var sharedContext: NSManagedObjectContext =  {
+        return CoreDataStackManager.sharedInstance().managedObjectContext!
+    }()
+
+
+}
+
+
+extension MapViewController : MKMapViewDelegate {
+    func mapView(mapView: MKMapView!, regionDidChangeAnimated animated: Bool) {
+        saveMapRegion()
+    }
+}
+
+
---
+++ VirtualTourist/Pin.swift
@@ -0,0 +1,32 @@
+//
+//  Pin.swift
+//  
+//
+//  Created by Nawfal on 01/09/2015.
+//
+//
+
+
+import CoreData
+import MapKit
+
+class Pin: NSManagedObject {
+    
+    @NSManaged var latitude: CLLocationDegrees
+    @NSManaged var longitude: CLLocationDegrees
+    
+    override init(entity: NSEntityDescription, insertIntoManagedObjectContext context: NSManagedObjectContext?){
+        super.init(entity: entity, insertIntoManagedObjectContext: context)
+    }
+    
+    init(latitude: CLLocationDegrees, longitude: CLLocationDegrees, context: NSManagedObjectContext){
+        
+        let entity = NSEntityDescription.entityForName("Pin", inManagedObjectContext: context)!
+        
+        super.init(entity: entity, insertIntoManagedObjectContext: context)
+        
+        self.latitude = latitude
+        self.longitude = longitude
+    }
+   
+}
